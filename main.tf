68471553116c8fd7498da94c857506f90e673517
Nepalgulmi <168068654+Nepalgulmi@users.noreply.github.com>
2024-09-02 12:31:11 +0100

    creat security group

--- main.tf
+++ main.tf
@@ -14,11 +14,54 @@ data "aws_ami" "app_ami" {
   owners = ["979382823631"] # Bitnami
 }
 
+data "aws_vpc" "default" {
+  default = true
+}
+
 resource "aws_instance" "web" {
   ami           = data.aws_ami.app_ami.id
   instance_type = "t3.nano"
 
+vpc_security_group_ids = (aws_security_group_web.id)
+
   tags = {
     Name = "HelloWorld"
   }
+}
+
+resource "aws_security_group" "web" {
+  name        = "web"
+  description = "Allow http and https in. Allow everything out"
+
+  vpc_id = data.aws_vpc.default.id
+}
+
+resource "aws_security_group_rule" "web_http_in" {
+  type      = "ingress"
+  from_port = 80
+  to_port   = 80
+  portocol  = "tcp"
+  cidr_blocks = ["0.0.0.0/0"]
+
+  security_group_id = aws_security_group_web_id
+}
+
+resource "aws_security_group_rule" "web_https_in" {
+  type      = "ingress"
+  from_port = 443
+  to_port   = 443
+  portocol  = "tcp"
+  cidr_blocks = ["0.0.0.0/0"]
+
+  security_group_id = aws_security_group_web_id
+}
+
+resource "aws_security_group_rule" "web_everything_out" {
+  type      = "egress"
+  from_port = 0
+  to_port   = 0
+  portocol  = "-1"
+  cidr_blocks = ["0.0.0.0/0"]
+
+  security_group_id = aws_security_group_web_id
 }
