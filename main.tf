98fbf69823bd3be726c484f14554dda1e68e99dd
Nepalgulmi <168068654+Nepalgulmi@users.noreply.github.com>
2024-09-02 12:34:26 +0100

    creat security group

--- main.tf
+++ main.tf
@@ -22,46 +22,46 @@ resource "aws_instance" "web" {
   ami           = data.aws_ami.app_ami.id
   instance_type = "t3.nano"
 
-vpc_security_group_ids = (aws_security_group_web.id)
+vpc_security_group_ids = (aws_security_group_blog.id)
 
   tags = {
     Name = "HelloWorld"
   }
 }
 
-resource "aws_security_group" "web" {
+resource "aws_security_group" "blog" {
   name        = "web"
   description = "Allow http and https in. Allow everything out"
 
   vpc_id = data.aws_vpc.default.id
 }
 
-resource "aws_security_group_rule" "web_http_in" {
+resource "aws_security_group_rule" "blog_http_in" {
   type      = "ingress"
   from_port = 80
   to_port   = 80
   portocol  = "tcp"
   cidr_blocks = ["0.0.0.0/0"]
 
-  security_group_id = aws_security_group_web_id
+  security_group_id = aws_security_group_blog_id
 }
 
-resource "aws_security_group_rule" "web_https_in" {
+resource "aws_security_group_rule" "blog_https_in" {
   type      = "ingress"
   from_port = 443
   to_port   = 443
   portocol  = "tcp"
   cidr_blocks = ["0.0.0.0/0"]
 
-  security_group_id = aws_security_group_web_id
+  security_group_id = aws_security_group_blog_id
 }
 
-resource "aws_security_group_rule" "web_everything_out" {
+resource "aws_security_group_rule" "blog_everything_out" {
   type      = "egress"
   from_port = 0
   to_port   = 0
   portocol  = "-1"
   cidr_blocks = ["0.0.0.0/0"]
 
-  security_group_id = aws_security_group_web_id
+  security_group_id = aws_security_group_blog_id
 }
